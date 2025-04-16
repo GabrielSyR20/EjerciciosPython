db8997ed05b8bdebb55c304253c18f9e8de10717
GabrielSyR20 <gabriel.benja.syr@gmail.com>
2025-04-16 19:44:32 -0300

    agregue cositas

--- Calse2.py
+++ Calse2.py
@@ -3,3 +3,11 @@ nombre = "patricio"
 
 print(nombre)
 print(edad)
+
+print(f"Hola {nombre}, tienes {edad} años")
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+print("---------------------------------------------------------")
